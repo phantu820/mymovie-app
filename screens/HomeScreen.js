afb95d88dceb680f6b0eda194a0e9febd59ef9b5
tudeptrai <tu416164@gmail.com>
2023-12-10 16:50:06 +0700

    update home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import { View, Text, TouchableOpacity, ScrollView, Platform } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
-import TrendingMovies from '../components/trendingMovies';
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+import TrendingMovies from '../components/TrendingMovies';
 import MovieList from '../components/movieList';
 import { StatusBar } from 'expo-status-bar';
 import { fetchTopRatedMovies, fetchTrendingMovies, fetchUpcomingMovies } from '../api/moviedb';
@@ -10,7 +10,6 @@ import { useNavigation } from '@react-navigation/native';
 import Loading from '../components/loading';
 import { styles } from '../theme';
 
-const ios = Platform.OS === 'ios';
 
 export default function HomeScreen() {
 
@@ -21,73 +20,71 @@ export default function HomeScreen() {
   const navigation = useNavigation();
 
 
-  useEffect(()=>{
+  useEffect(() => {
     getTrendingMovies();
     getUpcomingMovies();
     getTopRatedMovies();
-  },[]);
+  }, []);
 
-  const getTrendingMovies = async ()=>{
+  const getTrendingMovies = async () => {
     const data = await fetchTrendingMovies();
     console.log('got trending', data.results.length)
-    if(data && data.results) setTrending(data.results);
+    if (data && data.results) setTrending(data.results);
     setLoading(false)
   }
-  const getUpcomingMovies = async ()=>{
+  const getUpcomingMovies = async () => {
     const data = await fetchUpcomingMovies();
     console.log('got upcoming', data.results.length)
-    if(data && data.results) setUpcoming(data.results);
+    if (data && data.results) setUpcoming(data.results);
   }
-  const getTopRatedMovies = async ()=>{
+  const getTopRatedMovies = async () => {
     const data = await fetchTopRatedMovies();
     console.log('got top rated', data.results.length)
-    if(data && data.results) setTopRated(data.results);
+    if (data && data.results) setTopRated(data.results);
   }
-
-
 
   return (
     <View className="flex-1 bg-neutral-800">
       {/* search bar */}
-      <SafeAreaView className={ios? "-mb-2": "mb-3"}>
+      <SafeAreaView className="mb-3">
         <StatusBar style="light" />
         <View className="flex-row justify-between items-center mx-4">
           <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
-          <Text 
+          <Text
             className="text-white text-3xl font-bold">
-              <Text style={styles.text}>M</Text>ovies
+            <Text style={styles.text}>M</Text>ovies
           </Text>
-          <TouchableOpacity onPress={()=> navigation.navigate('Search')}>
+          <TouchableOpacity onPress={() => navigation.navigate('Search')}>
             <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
           </TouchableOpacity>
         </View>
       </SafeAreaView>
       {
-        loading? (
+        loading ? (
           <Loading />
-        ):(
-          <ScrollView 
-            showsVerticalScrollIndicator={false} 
-            contentContainerStyle={{paddingBottom: 10}}
+        ) : (
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 10 }}
           >
 
             {/* Trending Movies Carousel */}
-            { trending.length>0 && <TrendingMovies data={trending} /> }
+            {trending.length > 0 && <TrendingMovies data={trending} />}
 
             {/* upcoming movies row */}
-            { upcoming.length>0 && <MovieList title="Upcoming" data={upcoming} /> }
-            
+            {upcoming.length > 0 && <MovieList title="Upcoming" data={upcoming} />}
+
 
             {/* top rated movies row */}
-            { topRated.length>0 && <MovieList title="Top Rated" data={topRated} /> }
+            {topRated.length > 0 && <MovieList title="Top Rated" data={topRated} />}
 
           </ScrollView>
         )
       }
-      
-  </View>
-      
 
-   
+    </View>
+
+
+
   )
 }
